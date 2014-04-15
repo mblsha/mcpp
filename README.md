1146a3933a7c1b4bdf8cfcea9313dfec1e1d083f
Michail Pishchagin <mblsha@yandex-team.ru>
2014-04-15 15:29:51 +0400

    Lock formula on 2.7.2 release by default.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Fork of http://mcpp.sourceforge.net with minor compatibility fixes.
 
 # Installation
 
-`brew install --HEAD https://github.com/mblsha/mcpp/raw/master/mcpp-mblsha.rb`
+`brew install https://github.com/mblsha/mcpp/raw/master/mcpp-mblsha.rb`
--- mcpp-mblsha.rb
+++ mcpp-mblsha.rb
@@ -3,6 +3,8 @@ require 'formula'
 class McppMblsha < Formula
   homepage 'https://github.com/mblsha/'
   head "https://github.com/mblsha/mcpp.git"
+  url 'https://github.com/mblsha/mcpp/archive/v2.7.2-mblsha.tar.gz'
+  sha1 '161ad053074fdb3f3a369e91089314405eee6e9f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
