f2b1fd8a081f12754179ee63e2129432ca900908
Michail Pishchagin <mblsha@yandex-team.ru>
2014-06-11 13:43:08 +0400

    Build Mac binary for Mac OS X 10.6+.

--- mcpp-mblsha.rb
+++ mcpp-mblsha.rb
@@ -7,6 +7,7 @@ class McppMblsha < Formula
   sha1 '161ad053074fdb3f3a369e91089314405eee6e9f'
 
   def install
+    ENV['MACOSX_DEPLOYMENT_TARGET'] = '10.6'
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-mcpplib"
